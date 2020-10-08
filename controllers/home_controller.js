b863c1525951cf94e96aa72f073471e433c61465
Nishant Gautam <eddienishant@gmail.com>
2020-10-08 23:28:47 +0530

    added controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req,res) {
+    return res.end('<h1> Express is up and running for our Social Media</h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// access the home_contoller.js in the controller folder
+const homeController = require('../controllers/home_controller')
+
+
 module.exports = router;
+router.get('/',homeController.home)
