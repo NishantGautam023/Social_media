7032f2778bd6aeccd7acc60f3f5e07bfaaaf9787
Nishant Gautam <eddienishant@gmail.com>
2020-10-08 21:45:42 +0530

    setting express router: adding express router

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express router 
+
+app.use('/', require('./routes/index'));
+console.log("route has been loaded")
+
+
+
+
+
+
 app.listen(port, function(err) {
     if(err) {
         console.log(`Error is ${error}`)
